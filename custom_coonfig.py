190ac4a75a19b41b346449e10fd458740e379144
Bharath Ambati <bharath.hussy@gmail.com>
2015-04-23 22:56:41 +0530

    Create custom_coonfig.py

---
+++ custom_coonfig.py
@@ -0,0 +1,11 @@
+####################################
+# Configuration File for Python Web Robot
+#
+# Developed by PirateSecurity
+####################################
+
+#Variable Section
+
+base_url="http://rguktnuz.in"
+
+file_types=file_types=["mp3","ico","pdf","avi","flv","mp4","png","PNG","JPEG","jpeg","jar","jpg","JPG","css",'gif','GIF',"zip","rar","ZIP",'RAR',"xls","odf","ppt","PPT","xlc","PDF"]
